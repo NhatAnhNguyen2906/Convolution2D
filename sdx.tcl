5f6af7a061e3209c41efe4b1fdfc98645eb77bd8
Nguyễn Nhật Anh <127937811+NhatAnhNguyen2906@users.noreply.github.com>
2025-05-03 16:11:08 +0700

    Add files via upload

---
+++ sdx.tcl
@@ -0,0 +1,12 @@
+# ==============================================================
+# File generated on Sat May 03 15:54:42 +0700 2025
+# Vivado(TM) HLS - High-Level Synthesis from C, C++ and SystemC v2018.3 (64-bit)
+# SW Build 2405991 on Thu Dec  6 23:38:27 MST 2018
+# IP Build 2404404 on Fri Dec  7 01:43:56 MST 2018
+# Copyright 1986-2018 Xilinx, Inc. All Rights Reserved.
+# ==============================================================
+add_files -tb ../../Convolution2D_tb.cpp -cflags { -Wno-unknown-pragmas}
+add_files Convolution2D.h
+add_files Convolution2D.cpp
+set_part xc7z020clg484-1
+create_clock -name default -period 10
